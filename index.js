2ae21c7562846fc5ba6d431275d272b2ad0da4d4
unforgitful <dunkfunker@gmail.com>
2025-02-15 18:08:44 +0800

    Added Glowing Effect

--- index.js
+++ index.js
@@ -24,10 +24,20 @@
 
         if (timeLeft < 0) {
             const myCountdown = document.getElementById("countdown");
+            let bdayMessage = document.querySelector("h1");
+            let keyFrames = document.createElement("style");
+            document.head.appendChild(keyFrames);
+            keyFrames.innerHTML = `@keyframes glowEffect {
+        0% { text-shadow: 0 0 5px blue, 0 0 10px blue, 0 0 15px blue, 0 0 20px blue; }
+        50% { text-shadow: 0 0 10px rgb(61, 87, 255), 0 0 20px rgb(61, 87, 255), 0 0 30px rgb(61, 87, 255), 0 0 40px rgb(61, 87, 255); }
+        100% { text-shadow: 0 0 5px blue, 0 0 10px blue, 0 0 15px blue, 0 0 20px blue; }
+    }`;
+            
 
             clearInterval(countdownInterval);
             document.querySelector("h1").innerHTML = "Happy Birthday, Mark!";
-            myCountdown.style.visibility = "hidden";
+            bdayMessage.style.cssText = "padding: 60px; font-size: 6vw; animation: glowEffect 1.5s ease-in-out infinite;";
+            myCountdown.style.display = "none";
         }
     }
 
